c4e433d6f70c1f0f4ef46c2b5a50927167a5aeaf
Patrick Williamson <patrickw9080@outlook.com>
2021-09-26 16:54:08 -0400

    Added register route with test DB select.

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var dbconfig = require("../config/dbconfig");
+var dbConnection = dbconfig.getDb();
+var Request = require("tedious").Request;
+
+
+router.post("/register", function (req, res, next) {
+  const TAG = "users.js - post(/), ";
+  console.log(TAG + "body: ");
+  console.log(req.body);
+
+  console.log(process.env.FOO)
+
+  let sqlTestStatement = "SELECT * FROM SmiBuilder.Users";
+  var request = new Request(sqlTestStatement, function (err, rowCount, rows) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("SQL Total Rows: " + rowCount);
+      console.log(rows);
+    };
+  });
+  dbConnection.execSql(request)
+
+  res.send("Yeah!")
+});
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 
 module.exports = router;
