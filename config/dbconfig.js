c339cbf04c5f20257e3761d4bee2e8d5e59a196b
Patrick Williamson <patrickw9080@outlook.com>
2021-10-17 16:24:06 -0400

    Updated dbconfig functions to return promises.

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,75 +3,90 @@ var Connection = require("tedious").Connection;
 var Request = require("tedious").Request;
 const TYPES = require("tedious").TYPES;
 
-// Vars
-var dbConnectionVar;
-var usernameAppendNumber;
-
-function connectToDb() {
-    const SELF = this;
-    // Tedious
-    var config = {
-        server: process.env.DB_HOST,
-        authentication: {
-            type: "default",
+/**
+ * Wrap a Tedious connection in a Promise so it can be used async.
+ * Allows other .js files to call asyncConnectToDb(), run their SQL, then disconnect without copying major boilerplate code everywhere.
+ * @returns Promise resolved or rejected.
+ */
+function asyncConnectToDb() {
+    const TAG = "\dbconfig.js - asyncConnectToDb(), ";
+    let dbConnectionVar;
+    console.log(TAG + "Starting.");
+    return new Promise((resolve, reject) => {
+        var config = {
+            server: process.env.DB_HOST,
+            authentication: {
+                type: "default",
+                options: {
+                    userName: process.env.DB_USER,
+                    password: process.env.DB_PASS,
+                }
+            },
             options: {
-                userName: process.env.DB_USER,
-                password: process.env.DB_PASS,
+                database: process.env.DB_DATABASE,
+                rowCollectionOnRequestCompletion: true,
             }
-        },
-        options: {
-            database: process.env.DB_DATABASE,
-            rowCollectionOnRequestCompletion: true,
-        }
-    };
-    dbConnectionVar = new Connection(config);
-    dbConnectionVar.on("connect", function (err) {
-        if (err) {
+        };
+        dbConnectionVar = new Connection(config);
+        dbConnectionVar.on("connect", function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Database connection.")
+                resolve({ conStatus: "Success", resData: dbConnectionVar });
+            };
+        });
+        dbConnectionVar.on("databaseChange", function (databaseName) {
+            if (databaseName) {
+                console.log("Database changed. Name: ");
+                console.log(databaseName)
+            } else {
+                console.log("Database change had no name.")
+            };
+        });
+        dbConnectionVar.on("error", function (err) {
+            console.log("Tedious error: ");
             console.log(err);
-        } else {
-            console.log("\nDatabase connection.")
-            SELF.getAndSetUsernameAppend();
-        };
+            reject({ conStatus: "Failure", resData: err.message });
+        });
+        // Initialize the connection.
+        dbConnectionVar.connect();
     });
-    dbConnectionVar.on("databaseChange", function (databaseName) {
-        if (databaseName) {
-            console.log("\nDatabase changed. Name: ");
-            console.log(databaseName)
-        } else {
-            console.log("Database change had no name.")
-        };
-    });
-    dbConnectionVar.on("error", function (err) {
-        console.log("Tedious error: ");
-        console.log(err);
-    });
-    // Initialize the connection.
-    dbConnectionVar.connect();
 };
 
 
-function getAndSetUsernameAppend() {
+
+/**
+ * Get the count of current users, return 100 + that count as a string.
+ * Ex. if there's 2 users registered, return '102'. This number is added to the end of usernames.
+ * 
+ * This probably implodes if two users register at the exact same millisecond. I'll probably switch this abomination out
+ * for UUID.
+ * @param {Connection} dbConnectionVar 
+ * @returns Promise resolve or reject.
+ */
+function asyncGetAndSetUsernameAppend(dbConnectionVar) {
     const TAG = "\ndbconfig.js - getAndSetUsernameAppend(), "
     const SELF = this;
-    let sqlUserCountStatement = "SELECT COUNT(id) FROM SmiBuilder.Users";
-    let request = new Request(sqlUserCountStatement, function (err, rowCount, rows) {
-        console.log(TAG + "count:", rows[0][0].value);
-        appendNumber = 100 + rows[0][0].value;
-        SELF.usernameAppendNumber = String(appendNumber);
-        console.log("usernameAppendNumber: ", SELF.usernameAppendNumber);
-    });
-    dbConnectionVar.execSql(request);
+    return new Promise((resolve, reject) => {
+        let sqlUserCountStatement = "SELECT COUNT(id) FROM SmiBuilder.Users";
+        let request = new Request(sqlUserCountStatement, function (err, rowCount, rows) {
+            try {
+                console.log(TAG + "count:", rows[0][0].value);
+                appendNumber = 100 + rows[0][0].value;
+                SELF.usernameAppendNumber = String(appendNumber);
+                console.log("usernameAppendNumber: ", SELF.usernameAppendNumber);
+                resolve({ resStatus: "Success", resData: SELF.usernameAppendNumber });
+            } catch (error) {
+                console.log("ERROR: ");
+                console.log(error.message);
+                reject({ resStatus: "Failure", resData: error.message });
+            }
+        });
+        dbConnectionVar.execSql(request);
+    })
 };
 
-function getDb() {
-    return dbConnectionVar;
-};
 
-
-function incrementUsernameAppend() {
-    appendNumber = parseInt(this.usernameAppendNumber) + 1;
-    this.usernameAppendNumber = String(appendNumber)
-}
-
-module.exports = { connectToDb, getAndSetUsernameAppend, getDb, usernameAppendNumber, incrementUsernameAppend };
+module.exports = { asyncConnectToDb, asyncGetAndSetUsernameAppend, };
 
