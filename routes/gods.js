474e4d794504ededac7b7851b1587367510a3482
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-10-21 16:21:05 -0400

    Added get route to get a single god.

--- routes/gods.js
+++ routes/gods.js
@@ -25,6 +25,36 @@ router.get('/', async function (req, res, next) {
     res.json(response);
 });
 
+/**
+ * Get 1 god according to the id param.
+ */
+router.get("/:id", async function (req, res, next) {
+    const TAG = "\ngods.js - get(/get), ";
+    const godsArray = loadGods.getArrayOfGods();
+    const idToGet = parseInt(req.params.id)
+    let response = {};
+
+    console.log(TAG + "Getting 1 god");
+    console.dir(idToGet);
+
+    try {
+        let foundGod = godsArray.filter(god => { return parseInt(god.id) === idToGet });
+        if (foundGod.length !== 1) {
+            throw new Error(`Something went wrong. Found under 1, or more than 1 gods with the ID ${idToGet}.`)
+        } else {
+            response = { status: "Success", resData: foundGod[0] };
+        }
+    } catch (error) {
+        console.log("ERROR: ");
+        console.log(error.message);
+        console.log(error);
+        response = { status: "Failure", resData: error.message };
+    }
+
+    res.json(response);
+
+});
+
 
 
 module.exports = router;
