ddda4eb3e2f4ff95879087bc4474d94eae224f78
PatWStudentGit2000 <79163131+PatWStudentGit2000@users.noreply.github.com>
2021-11-07 12:30:46 -0500

    Added updated filtering when doing the initial item retrieval.

--- routes/items.js
+++ routes/items.js
@@ -14,8 +14,13 @@ router.get("/", async function (req, res, next) {
 
     console.log(TAG + "Sending array of all the items.");
 
+
+
     try {
-        response = { status: "Success", resData: itemsArray };
+        let filteredItemsArray = itemsArray.filter(item => {
+            return item.ActiveFlag == "y"
+        });
+        response = { status: "Success", resData: filteredItemsArray };
     } catch (error) {
         console.log("ERROR: ");
         console.log(error.message);
@@ -74,15 +79,24 @@ router.get("/relics", async function (req, res, next) {
     res.json(response);
 });
 
-router.get("/:godRole/:godDamageType/:godBasicAttackType/:itemsOnly", async function (req, res, next) {
+router.get("/:godRole/:godDamageType/:godBasicAttackType/:itemsOnly/:isRatatoskr", async function (req, res, next) {
     const TAG = "\nitems.js - get(/godRole/godDamageType/godBasicAttackType/itemsOnly), ";
     const itemsArray = loadItems.getArrayOfItems();
     const { params } = req;
-    const { godRole, godDamageType, godBasicAttackType, itemsOnly } = params
+    const { godRole, godDamageType, godBasicAttackType, itemsOnly, isRatatoskr } = params
     let response = {};
+
+    // These can catch most items
+    const magicalRelatedItemKeys = ["Magical Power", "Magical Penetration", "Magical Lifesteal"];
+    const physicalRelatedItemKeys = ["Physical Power", "Physical Penetration", "Physical Lifesteal"]
+
+    // These are more specific arrays for items that don't get caught by the above arrays.
+    const physicalOnlyItems = [10662];
+    const magicalOnlyItems = [];
 
     console.log(TAG + "Getting all regular items that this god can use.");
     console.log(`Role: ${godRole}, Damage Type: ${godDamageType}, Basic Attack Type: ${godBasicAttackType}, Only get items: ${itemsOnly}`);
+    console.log(`Is this God Ratatoskr: ${isRatatoskr}`);
 
     try {
         // First filter out all inactive items
@@ -97,12 +111,71 @@ router.get("/:godRole/:godDamageType/:godBasicAttackType/:itemsOnly", async func
             });
         };
 
+
+
         // Third filter out items that cannot be used by this role
         filteredItemsArray = filteredItemsArray.filter(item => {
             let restrictedRoles = item.RestrictedRoles.split(",");
             restrictedRoles.forEach((roleString, index) => restrictedRoles[index] = roleString.toLowerCase());
             return restrictedRoles.includes(godRole.toLowerCase()) !== true;
         });
+
+
+        // Fourth is damage type filters.
+        filteredItemsArray = filteredItemsArray.filter(item => {
+            if (item.StartingItem === true) {
+                return true;
+            } else {
+                if (godDamageType === "Physical") {
+                    let index = 0;
+                    let aLength = item.ItemDescription.Menuitems.length;
+                    while (index < aLength) {
+                        if (magicalRelatedItemKeys.includes(item.ItemDescription.Menuitems[index].Description)) {
+                            return false;
+                        };
+                        index++;
+                    };
+                    return true;
+                } else if (godDamageType === "Magical") {
+                    let index = 0;
+                    let aLength = item.ItemDescription.Menuitems.length;
+                    while (index < aLength) {
+                        if (physicalRelatedItemKeys.includes(item.ItemDescription.Menuitems[index].Description)) {
+                            return false;
+                        };
+                        index++;
+                    };
+                    return true;
+                };
+            }
+        });
+
+        // Fifth is to catch items that evade the above filters
+        filteredItemsArray = filteredItemsArray.filter(item => {
+            if (godDamageType === "Physical") {
+                if (magicalOnlyItems.includes(item.ItemId) || magicalOnlyItems.includes(item.RootItemId)) {
+                    return false;
+                };
+                return true;
+            } else if (godDamageType === "Magical") {
+                if (physicalOnlyItems.includes(item.ItemId) || physicalOnlyItems.includes(item.RootItemId)) {
+                    return false;
+                };
+                return true;
+            };
+        });
+
+        // FINAL filter catches character specific edge cases.
+        // IIRC there's only one in the game right now: Rats acorn. Nobody but Rat can use the acorn.
+        filteredItemsArray = filteredItemsArray.filter(item => {
+            // Deny the Acorn tree if current god is not Ratatoskr
+            if (item.RootItemId === 18703 && isRatatoskr === "false") {
+                return false;
+            }
+            return true;
+        });
+
+
         response = { status: "Success", resData: filteredItemsArray };
     } catch (error) {
         console.log("ERROR: ");
